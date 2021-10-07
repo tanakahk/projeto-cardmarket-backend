bc40748b3553c706747e52405b0177a8d6c88aac
tanakahk <74397585+tanakahk@users.noreply.github.com>
2021-10-07 04:11:36 -0300

    trx_sr update
    agora quando vc vender uma skill, ele vai buscar a primeira skill sua e mandar um timestamp para a coluna deleted_at para marcar como skill vendida. Ele faz isso para a table user_sr e billing para garantir a venda.

--- src/controllers/trx/postTrxSr.js
+++ src/controllers/trx/postTrxSr.js
@@ -20,12 +20,6 @@ const validaEntry = (req, user, sr, response) => {
     response.status = "Skill inexistente"
     return false
   }
-  if (req.trx_type === 0 || req.trx_type === 1) { }
-  else {
-    response.status = "trx_type inválido / inexistente. Mande 1 para comprar ou 0 para vender."
-    return false
-  }
-
   return true
 }
 
@@ -37,32 +31,60 @@ const postTrxSr = async (req, res) => {
 
   if (!validaEntry(req.body, user, sr, response)) {
     return res.status(400).json(response)
-  }
-
-  if (user && sr) {
+  } else {
     const newUserSr = await UserSr.query().insert({
       user_id: user['id'],
       sr_id: sr['id']
     })
     if (newUserSr instanceof UserSr) {
+      switch (req.body.trx_type) {
+        case 1:
+          console.log("comprando");
+          const newBillingBuy = await Billing.query().insert({
+            trx_type: req.body.trx_type,
+            amount: sr['price'],
+            user_id: user['id'],
+            user_sr_id: newUserSr['id']
+          })
 
-      const newBilling = await Billing.query().insert({
-        trx_type: req.body.trx_type,
-        amount: sr['price'],
-        user_id: user['id'],
-        user_sr_id: newUserSr['id']
-      })
+          if (newBillingBuy instanceof Billing) {
+            response.status = "Skill comprada"
+            return res.status(200).json(response)
+          }
+          break
+        case 0:
+          console.log("vendendo");
+          const newBillingSell = await Billing.query().insert({
+            trx_type: req.body.trx_type,
+            amount: sr['price'],
+            user_id: user['id'],
+            user_sr_id: newUserSr['id']
+          })
 
-      if (newBilling instanceof Billing) {
-        if (req.body.trx_type === 1) {
-          response.status = "Skill comprada"
-        }
-        response.status = "Skill vendida"
-        return res.status(200).json(response)
+          // find first buyed sr on UserSr by user_id and sr_id
+          const buyedUserSr = await UserSr.query().where({ user_id: user['id'], sr_id: sr['id'] }).whereNull('deleted_at').first()
+
+          if (buyedUserSr instanceof UserSr) {
+            // set deleted_at column on UserSr by id
+            const setUserSrDeletedDate = await UserSr.query().patch({ deleted_at: new Date() }).findById(buyedUserSr.id)
+
+            if (setUserSrDeletedDate) {
+              // set deleted_at column on Billing by user_sr_id
+              const setBillingDeletedDate = await Billing.query().where({ user_sr_id: buyedUserSr.id }).patch({ deleted_at: new Date() })
+
+              if (setBillingDeletedDate) {
+                response.status = "Skill vendida"
+                return res.status(200).json(response)
+              }
+            }
+          }
+          break
+        default:
+          response.status = "trx_type inválido / inexistente. Mande 1 para comprar ou 0 para vender."
+          return res.status(400).json(response)
       }
     }
   }
-
   return res.status(400).json(response)
 }
 
